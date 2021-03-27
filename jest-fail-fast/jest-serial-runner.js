bdfc0710abba4c20e4e8a4f24f1896ee7a812f19
ppiyush13 <ppiyush13@gmail.com>
2021-03-27 20:50:09 +0530

    host jest-fail-fast seperately

--- jest-fail-fast/jest-serial-runner.js
+++
@@ -1,9 +0,0 @@
-
-const TestRunner = require('jest-circus/runner');
-
-module.exports = class SerialRunner extends TestRunner {
-    constructor(...attr) {
-        super(...attr)
-        this.isSerial = true
-    }
-}
