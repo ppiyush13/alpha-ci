99d78bc89be0d9835811ce8978cfed87d7207774
ppiyush13 <ppiyush13@gmail.com>
2021-03-29 10:52:31 +0530

    fixturize demo-pkg

--- e2e/scripts/setup-test-dir.js
+++ e2e/scripts/setup-test-dir.js
@@ -1,18 +1,11 @@
 import tmp from 'tmp';
-import exitHook from 'exit-hook';
-import shell from 'shelljs';
 
 export const setupTestDir = () => {
-    const { name, removeCallback } = tmp.dirSync({ 
+    const { name: dirName } = tmp.dirSync({ 
         prefix: 'demo-pkg', 
         unsafeCleanup: true, 
         discardDescriptor: true 
     });
 
-    exitHook(() => {
-        shell.cd('/');
-        removeCallback();
-    });
-
-    return name;
+    return dirName;
 };
